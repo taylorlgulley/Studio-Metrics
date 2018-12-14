91b615717e20c5008858da5d418548251ce0b31e
Taylor Gulley <taylorlgulley@yahoo.com>
2018-12-14 13:36:42 -0600

    the create is working and will create the joiner tables for artists and players

--- StudioMetrics/Controllers/ProjectsController.cs
+++ StudioMetrics/Controllers/ProjectsController.cs
@@ -144,19 +144,6 @@ namespace StudioMetrics.Controllers
                 Value = "0"
             });
 
-            // I dont't think these are needed since they will be a multi select and these will be nullable
-            //playerListOptions.Insert(0, new SelectListItem
-            //{
-            //    Text = "Select Players",
-            //    Value = "0"
-            //});
-
-            //artistListOptions.Insert(0, new SelectListItem
-            //{
-            //    Text = "Select Artists",
-            //    Value = "0"
-            //});
-
             createViewModel.ProjectTypes = projectTypeListOptions;
             createViewModel.StatusTypes = statusTypeListOptions;
             createViewModel.Clients = clientListOptions;
@@ -172,19 +159,128 @@ namespace StudioMetrics.Controllers
         [HttpPost]
         [Authorize]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ProjectId,Title,ProjectTypeId,Description,Payrate,TimeTable,StartDate,StatusTypeId,ClientId,UserId")] Project project)
+        public async Task<IActionResult> Create(ProjectCreateViewModel createProject)
         {
+            ModelState.Remove("Project.User");
+            ModelState.Remove("Project.UserId");
             if (ModelState.IsValid)
             {
-                _context.Add(project);
+                createProject.Project.User = await GetCurrentUserAsync();
+                createProject.Project.UserId = createProject.Project.User.Id;
+                _context.Add(createProject.Project);
+                // A foreach looping through the SelectedPlayers to create the instances of the PlayerProjects for every one in the list. Then adding them to the database. The foreach works because each entry in the list is an int representing the playerId then it takes the project Id from the created project in the model to make the StudentExercise. Needs to be in an if statement so a person can create a project and not assign them an project.
+                if (createProject.SelectedPlayers != null)
+                {
+                    foreach (int playerId in createProject.SelectedPlayers)
+                    {
+                        PlayerProject newPP = new PlayerProject()
+                        {
+                            PlayerId = playerId,
+                            ProjectId = createProject.Project.ProjectId
+                        };
+                        _context.Add(newPP);
+                    }
+                }
+                if (createProject.SelectedArtists != null)
+                {
+                    foreach (int artistId in createProject.SelectedArtists)
+                    {
+                        ArtistProject newAP = new ArtistProject()
+                        {
+                            ArtistId = artistId,
+                            ProjectId = createProject.Project.ProjectId
+                        };
+                        _context.Add(newAP);
+                    }
+                }
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["ClientId"] = new SelectList(_context.Client, "ClientId", "Email", project.ClientId);
-            ViewData["ProjectTypeId"] = new SelectList(_context.ProjectType, "ProjectTypeId", "Type", project.ProjectTypeId);
-            ViewData["StatusTypeId"] = new SelectList(_context.StatusType, "StatusTypeId", "Type", project.StatusTypeId);
-            ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", project.UserId);
-            return View(project);
+
+            var user = await GetCurrentUserAsync();
+
+            var projectTypes = await _context.ProjectType.ToListAsync();
+            var statusTypes = await _context.StatusType.ToListAsync();
+            var clients = await _context.Client.Where(c => c.User == user).ToListAsync();
+            var players = await _context.Player.Where(c => c.User == user).ToListAsync();
+            var artists = await _context.Artist.Where(c => c.User == user).ToListAsync();
+
+            var projectTypeListOptions = new List<SelectListItem>();
+            var statusTypeListOptions = new List<SelectListItem>();
+            var clientListOptions = new List<SelectListItem>();
+            var playerListOptions = new List<SelectListItem>();
+            var artistListOptions = new List<SelectListItem>();
+
+            foreach (ProjectType pt in projectTypes)
+            {
+                projectTypeListOptions.Add(new SelectListItem
+                {
+                    Value = pt.ProjectTypeId.ToString(),
+                    Text = pt.Type
+                });
+            }
+
+            foreach (StatusType st in statusTypes)
+            {
+                statusTypeListOptions.Add(new SelectListItem
+                {
+                    Value = st.StatusTypeId.ToString(),
+                    Text = st.Type
+                });
+            }
+
+            foreach (Client c in clients)
+            {
+                clientListOptions.Add(new SelectListItem
+                {
+                    Value = c.ClientId.ToString(),
+                    Text = c.Name
+                });
+            }
+
+            foreach (Player p in players)
+            {
+                playerListOptions.Add(new SelectListItem
+                {
+                    Value = p.PlayerId.ToString(),
+                    Text = p.FirstName + " " + p.LastName
+                });
+            }
+
+            foreach (Artist a in artists)
+            {
+                artistListOptions.Add(new SelectListItem
+                {
+                    Value = a.ArtistId.ToString(),
+                    Text = a.Name
+                });
+            }
+
+            projectTypeListOptions.Insert(0, new SelectListItem
+            {
+                Text = "Choose a Project Type",
+                Value = "0"
+            });
+
+            statusTypeListOptions.Insert(0, new SelectListItem
+            {
+                Text = "Choose a Status",
+                Value = "0"
+            });
+
+            clientListOptions.Insert(0, new SelectListItem
+            {
+                Text = "Choose a Client",
+                Value = "0"
+            });
+
+            createProject.ProjectTypes = projectTypeListOptions;
+            createProject.StatusTypes = statusTypeListOptions;
+            createProject.Clients = clientListOptions;
+            createProject.AvailablePlayers = playerListOptions;
+            createProject.AvailableArtists = artistListOptions;
+
+            return View(createProject);
         }
 
         // GET: Projects/Edit/5
